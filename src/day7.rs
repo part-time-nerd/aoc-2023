782009033bcebc9cb72e8dcc74d966402185e28f
Jacob Bush <jacobbush9@gmail.com>
2023-12-07 13:45:29 -0500

    more efficient solution using caching for Day 7 Part 1

--- src/day7.rs
+++ src/day7.rs
@@ -1,20 +1,42 @@
 use anyhow::{anyhow, Error, Result};
 use std::{cmp::Ordering, collections::HashMap, str::FromStr};
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Copy, Clone, Hash)]
-struct Card(u8);
+// #[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Copy, Clone, Hash)]
+// struct Card(u8);
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Clone, Copy, Hash)]
+enum Card {
+    Two = 2,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
+}
 
 impl From<char> for Card {
     fn from(value: char) -> Self {
         match value {
-            '0' => panic!("Card can't be 0"),
-            '1' => panic!("Card can't be 1"),
-            c if c.is_ascii_digit() => Card(c.to_digit(10).unwrap() as u8),
-            'T' => Card(10),
-            'J' => Card(11),
-            'Q' => Card(12),
-            'K' => Card(13),
-            'A' => Card(14),
+            '2' => Card::Two,
+            '3' => Card::Three,
+            '4' => Card::Four,
+            '5' => Card::Five,
+            '6' => Card::Six,
+            '7' => Card::Seven,
+            '8' => Card::Eight,
+            '9' => Card::Nine,
+            'T' => Card::Ten,
+            'J' => Card::Jack,
+            'Q' => Card::Queen,
+            'K' => Card::King,
+            'A' => Card::Ace,
             _ => panic!("Card can't be {value}"),
         }
     }
@@ -23,21 +45,14 @@ impl From<char> for Card {
 #[derive(PartialEq, Eq, Debug)]
 struct Hand {
     cards: [Card; 5],
+    hand_type: HandType,
     bid: usize,
 }
 
 impl Ord for Hand {
     fn cmp(&self, other: &Self) -> Ordering {
-        match self.hand_type().cmp(&other.hand_type()) {
-            Ordering::Equal => {
-                for (c1, c2) in self.cards.iter().zip(other.cards) {
-                    match c1.cmp(&c2) {
-                        Ordering::Equal => continue,
-                        ordering => return ordering,
-                    }
-                }
-                Ordering::Equal
-            }
+        match self.hand_type.cmp(&other.hand_type) {
+            Ordering::Equal => self.cards.cmp(&other.cards),
             ordering => ordering,
         }
     }
@@ -49,49 +64,22 @@ impl PartialOrd for Hand {
     }
 }
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd)]
 enum HandType {
-    FiveOfAKind,
-    FourOfAKind,
-    FullHouse,
-    ThreeOfAKind,
-    TwoPair,
-    OnePair,
     HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
 }
 
-impl Into<u8> for &HandType {
-    fn into(self) -> u8 {
-        match self {
-            HandType::FiveOfAKind => 6,
-            HandType::FourOfAKind => 5,
-            HandType::FullHouse => 4,
-            HandType::ThreeOfAKind => 3,
-            HandType::TwoPair => 2,
-            HandType::OnePair => 1,
-            HandType::HighCard => 0,
-        }
-    }
-}
-
-impl Ord for HandType {
-    fn cmp(&self, other: &Self) -> Ordering {
-        let _self: u8 = self.into();
-        _self.cmp(&other.into())
-    }
-}
-
-impl PartialOrd for HandType {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Hand {
-    fn hand_type(&self) -> HandType {
+impl From<&[Card; 5]> for HandType {
+    fn from(cards: &[Card; 5]) -> Self {
         let mut counts: HashMap<Card, u8> = HashMap::default();
 
-        for card in self.cards.iter() {
+        for card in cards.iter() {
             if let Some(count) = counts.get_mut(card) {
                 *count += 1;
             } else {
@@ -101,12 +89,12 @@ impl Hand {
 
         match counts.len() {
             1 => HandType::FiveOfAKind,
-            2 => match counts.values().into_iter().collect::<Vec<&u8>>()[..] {
+            2 => match counts.values().collect::<Vec<&u8>>()[..] {
                 [3, 2] | [2, 3] => HandType::FullHouse,
                 [4, 1] | [1, 4] => HandType::FourOfAKind,
                 _ => panic!("Unexpected counts of cards"),
             },
-            3 => match counts.values().into_iter().collect::<Vec<&u8>>()[..] {
+            3 => match counts.values().collect::<Vec<&u8>>()[..] {
                 [2, 2, 1] | [2, 1, 2] | [1, 2, 2] => HandType::TwoPair,
                 [3, 1, 1] | [1, 3, 1] | [1, 1, 3] => HandType::ThreeOfAKind,
                 _ => panic!("Unexpected counts of cards"),
@@ -122,10 +110,8 @@ impl FromStr for Hand {
     type Err = Error;
     fn from_str(s: &str) -> Result<Self> {
         if let Some((cards, bid)) = s.split_once(' ') {
-            Ok(Self {
-                cards: cards.chars().map(|c| c.into()).collect::<Vec<Card>>()[..].try_into()?,
-                bid: bid.parse()?,
-            })
+            let cards: [Card; 5] = cards.chars().map(|c| c.into()).collect::<Vec<Card>>()[..].try_into()?;
+            Ok(Self { cards, bid: bid.parse()?, hand_type: (&cards).into() })
         } else {
             Err(anyhow!("Could not split the hand into a (cards, bid) tuple: {s}"))
         }
@@ -155,7 +141,14 @@ QQQJA 483
     #[test]
     fn test_hand_from_string() {
         let hand: Hand = "32T3K 765".parse().unwrap();
-        assert_eq!(hand, Hand{cards: [Card(3), Card(2), Card(10), Card(3), Card(13)], bid: 765 });
+        assert_eq!(
+            hand,
+            Hand {
+                cards: [Card::Three, Card::Two, Card::Ten, Card::Three, Card::King],
+                hand_type: HandType::OnePair,
+                bid: 765
+            }
+        );
     }
 
     #[test]
@@ -167,20 +160,22 @@ QQQJA 483
     }
 
     #[test]
-    fn test_hand_type() {
-        let h1 = Hand { cards: [Card(3), Card(4), Card(5), Card(6), Card(9)], bid: 0 };
-        let h2 = Hand { cards: [Card(2), Card(2), Card(3), Card(4), Card(5)], bid: 0 };
-        let h3 = Hand { cards: [Card(3), Card(3), Card(2), Card(3), Card(2)], bid: 0 };
-        assert_eq!(h1.hand_type(), HandType::HighCard);
-        assert_eq!(h2.hand_type(), HandType::OnePair);
-        assert_eq!(h3.hand_type(), HandType::FullHouse);
-    }
-
-    #[test]
     fn test_hand_ordering() {
-        let h1 = Hand { cards: [Card(3), Card(4), Card(5), Card(6), Card(9)], bid: 0 };
-        let h2 = Hand { cards: [Card(3), Card(4), Card(5), Card(8), Card(6)], bid: 0 };
-        let h3 = Hand { cards: [Card(2), Card(2), Card(3), Card(4), Card(5)], bid: 0 };
+        let h1 = Hand {
+            cards: [Card::Three, Card::Four, Card::Five, Card::Six, Card::Nine],
+            hand_type: HandType::HighCard,
+            bid: 0,
+        };
+        let h2 = Hand {
+            cards: [Card::Three, Card::Four, Card::Five, Card::Eight, Card::Six],
+            hand_type: HandType::HighCard,
+            bid: 0,
+        };
+        let h3 = Hand {
+            cards: [Card::Two, Card::Two, Card::Three, Card::Four, Card::Five],
+            hand_type: HandType::OnePair,
+            bid: 0,
+        };
         assert!(h1 == h1);
         assert!(h1 < h2);
         assert!(h3 > h1);
